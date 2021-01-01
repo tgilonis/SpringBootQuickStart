699fff8d6a96e4106563d4331e3fa39019a6a8fe
tgilonis <tgilonis@protonmail.com>
2021-01-01 14:28:39 +0000

    get,post,put and delete methods added to controller

--- course-api/src/main/java/com/tgilonis/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/com/tgilonis/springbootstarter/topic/TopicController.java
@@ -1,9 +1,7 @@
 package com.tgilonis.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
@@ -19,6 +17,7 @@ public class TopicController
     private TopicService topicService;
 
     @RequestMapping("/topics")
+    //default request is a get request, in the addTopic() method we can see how to specify the type of request
     public List<Topic> getAllTopics()
     {
         return topicService.getAllTopics();
@@ -29,5 +28,26 @@ public class TopicController
     //pathvariable annotation maps the id variable in the requestmapping to the paramater id
     {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic)
+    //RequestBody annotation used to grab the body from the post url and convert it into a topic
+    {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic,
+                            @PathVariable String id)
+    //RequestBody annotation used to grab the body from the post url and convert it into a topic
+    {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable String id)
+    {
+        topicService.deleteTopic(id);
     }
 }
--- course-api/src/main/java/com/tgilonis/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/com/tgilonis/springbootstarter/topic/TopicService.java
@@ -2,18 +2,21 @@ package com.tgilonis.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService
 {
+//when getting a list from Arrays.asList, you end up with an immutable list. As such, we need to new up
+    //an arraylist when using arrays.aslist
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javascript", "JavaScript", "JavaScript Description")
-        );
+        ));
 
     public List<Topic> getAllTopics()
     {
@@ -25,4 +28,28 @@ public class TopicService
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
     }
 
+    public void addTopic(Topic topic)
+    {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id,
+                            Topic topic)
+    {
+        for(int i = 0; i < topics.size(); i++)
+        {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id))
+            {
+                topics.set(i, topic);
+                return;
+            }
+        }
+
+    }
+
+    public void deleteTopic(String id)
+    {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
 }
